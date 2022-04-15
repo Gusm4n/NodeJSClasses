c32103d4193eaf1a2a64dd6e83f30aea367b23fb
gusm4n <gusmao.dev@gmail.com>
2022-04-15 16:24:07 -0300

    Aula de parâmetros NodeJS

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
+const e = require("express");
 const express = require("express"); //Importando o Express
 const app = express(); //Iniciando o Express
 
 app.get("/",function(req, res){
-    res.send("Seja bem vindo ao meu primeiro servidor NodeJS :D")
-});
+    res.send("<h1>Seja bem vindo ao meu primeiro servidor NodeJS :D</h1>")
+})
 
-app.get("/blog", function(req,res){
-    res.send("Bem vindo ao meu blog")
+app.get("/blog/:artigo?", function(req,res){
+
+    var artigo = req.params.artigo;
+
+    if(artigo){
+        res.send("<h2>Artigo: " + artigo + "</h2>")
+    }else{
+        res.send("Bem vindo ao meu blog")
+
+    }
+
 })
 
 app.get("/youtube", function(req,res){
     res.send("Faaaala pessoal, bem vindo ao meu canal. =)")
+});
+
+app.get("/ola/:nome/:empresa", function(req,res){
+    //REQ => Dados enviados pelo usuário
+    //RES => Resposta que será enviada p/ o usuário
+    var nome = req.params.nome;
+    var empresa = req.params.empresa;
+    res.send("<h1>Olá, " + nome + ',' + ' da empresa ' + empresa + "</h1>")
 })
 
 app.listen(4000, function(erro){
@@ -20,3 +38,4 @@ app.listen(4000, function(erro){
      console.log("Servidor iniciado com muito sucesso! (:")
  }
 });
+
