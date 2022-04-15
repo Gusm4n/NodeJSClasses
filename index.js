e4d895e12167584bdddc225fdf22fd29b1d676a3
gusm4n <gusmao.dev@gmail.com>
2022-04-15 16:34:20 -0300

    atualização - Parâmetros

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.get("/blog/:artigo?", function(req,res){
 
 })
 
+//Para criar um parâmetro opcional, basta seguir o exemplo da linha 9
+//Onde após os caminhos "/blog/:artigo", inserir um sinal de interrogação
+//Após isso, fazer uma validação, conforme se inicia na linha 13.
+
 app.get("/youtube", function(req,res){
     res.send("Faaaala pessoal, bem vindo ao meu canal. =)")
 });
@@ -31,11 +35,15 @@ app.get("/ola/:nome/:empresa", function(req,res){
     res.send("<h1>Olá, " + nome + ',' + ' da empresa ' + empresa + "</h1>")
 })
 
+//Para criarmos parâmetros no Node, precisamos seguir o exemplo da linha 30
+//Onde designamos o caminho "/ola/", após o '/', precisamos colocar dois pontos ':'
+//E em seguida, o parâmetro que desejamos inserir, conforme colocado no exemplo citado.
+
 app.listen(4000, function(erro){
  if(erro){
      console.log('Ocorreu um erro...')
  }else{
-     console.log("Servidor iniciado com muito sucesso! (:")
+     console.log("Servidor iniciado com muito sucesso!")
  }
 });
 
